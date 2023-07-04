c0d34031d359d83df5bb1a427b7d806a5837ab28
HalKiyo <hasegawa.k.ai@m.titech.ac.jp>
2023-07-04 23:33:03 +0900

    name changed

--- class/thailand/train.py
+++ class/thailand/train.py
@@ -9,58 +9,84 @@ import matplotlib.pyplot as plt
 
 from model3 import build_model
 from view import draw_val, show_class, view_accuracy
-from util import load, shuffle, mask
+from util import transfer_load, train_val_split, _mask
 from gradcam import grad_cam, show_heatmap, image_preprocess
 
 def main():
     train_flag = False
 
-    px = Pixel()
+    TRS = Transfer()
 
     if train_flag is True:
-        predictors, predictant = load(px.tors, px.tant)
-        px.training(*shuffle(predictors, predictant, px.vsample, px.seed, px.lat_grid, px.lon_grid))
-        print(f"{px.weights_dir}: SAVED")
-        print(f"{px.savefile}: SAVED")
-        px.validation()
+        predictors, predictant = transfer_load(TRS.new_tors, TRS.new_tand)
+        #px.training(*shuffle(predictors, predictant, px.vsample, px.seed, px.lat_grid, px.lon_grid))
+        print(f"{TRS.new_weights_dir}: SAVED")
+        print(f"{TRS.savefile}: SAVED")
     else:
         print(f"train_flag is {train_flag}: not saved")
 
-    px.show(val_index=px.val_index)
-    px.label_dist_multigrid()
+    TRS.validation()
+    TRS.show(val_index=TRS.val_index)
+    TRS.label_dist_multigrid()
 
     plt.show()
 
-class Pixel():
+class Transfer():
     def __init__(self):
-        self.val_index = 20 #true_index=330, false_index=20
+        ###################################################
+        # change here
+        ###################################################
+        self.val_index = 1 #true_index=330, false_index=20
         self.class_num = 5
+        self.new_epochs = 200
+        self.new_batch_size = 32
         self.descrete_mode = 'EFD'
-        self.epochs = 150
-        self.batch_size = 256
         self.resolution = '1x1' # 1x1 or 5x5_coarse
-        ########################### EDIT HERE
-        self.var_num = 4
-        self.tors = 'predictors_coarse_std_Apr_msot'
-        self.tant = f"pr_{self.resolution}_std_MJJASO_thailand_{self.descrete_mode}_{self.class_num}"
-        ############################
+        self.new_tors = 'tos_coarse_std_Apr'
+        self.new_tand = f"aphro_{self.resolution}_coarse_std_MJJASO_thailand_{self.descrete_mode}_{self.class_num}"
+        ###################################################
+        # do not change here
+        ###################################################
         self.seed = 1
-        self.vsample = 1000
+        self.old_epochs = 150
+        self.old_batch_size = 256
+        self.old_tors = 'predictors_coarse_std_Apr_msot'
+        self.old_tand = f"pr_{self.resolution}_std_MJJASO_thailand_{self.descrete_mode}_{self.class_num}"
+        self.old_weights_dir = f"/docker/mnt/d/research/D2/cnn3/weights/class/" \
+                               f"{self.old_tors}-{self.old_tand}"
+        ###################################################
+        # do not change here
+        ###################################################
+        self.var_num = 1
         self.lat, self.lon = 24, 72
         self.lat_grid, self.lon_grid = 20, 20
         self.grid_num = self.lat_grid*self.lon_grid 
         self.loss = tf.keras.losses.CategoricalCrossentropy()
         self.metrics = tf.keras.metrics.CategoricalAccuracy()
-        self.savefile = f"/docker/mnt/d/research/D2/cnn3/train_val/class/{self.tors}-{self.tant}.pickle"
-        self.weights_dir = f"/docker/mnt/d/research/D2/cnn3/weights/class/{self.tors}-{self.tant}"
-        self.result_dir = f"/docker/mnt/d/research/D2/cnn3/result/class/thailand/{self.resolution}/{self.tors}-{self.tant}"
-        self.result_path = self.result_dir + f"/class{self.class_num}_epoch{self.epochs}_batch{self.batch_size}_seed{self.seed}.npy"
+        #####################################################
+        # do not change here
+        ####################################################
+        self.train_val_paht = f"/docker/mnt/d/research/D3/cnn3/transfer/train_val/class/" \
+                              f"{self.new_tors}-{self.new_tand}.pickle"
+        self.new_weights_dir = f"/docker/mnt/d/research/D3/cnn3/transfer/weights/class/" \
+                               f"{self.new_tors}-{self.new_tand}"
+        self.result_dir = f"/docker/mnt/d/research/D3/cnn3/transfer/result/class/thailand/" \
+                          f"{self.resolution}/{self.new_tors}-{self.new_tand}"
+        self.result_path = f"{self.result_dir}" \
+                           f"/class{self.class_num}_epoch{self.new_epochs}_batch{self.new_batch_size}.npy"
 
-    def training(self, x_train, y_train, x_val, y_val, train_dct, val_dct):
-        x_train, x_val = mask(x_train), mask(x_val)
-        x_train, x_val = x_train.transpose(0, 2, 3, 1), x_val.transpose(0, 2, 3, 1)
-        y_train, y_val = y_train.reshape(len(y_train), self.grid_num), y_val.reshape(len(y_val), self.grid_num)
-        os.makedirs(self.weights_dir, exist_ok=True) # create weight directory
+    ###########################################################
+    # training begin
+    ##########################################################
+    def training(self, x_train, y_train, x_val, y_val, patience_num=1000):
+        x_train = _mask(x_train)
+        x_train = x_train[:, :, :, np.newaxis]
+        x_val = _mask(x_val)
+        x_val = x_val[:, :, :, np.newaxis]
+        y_train = y_train.reshape(len(y_train), self.grid_num)
+        y_val = y_val.reshape(len(y_val, self.grid_num))
+        os.makedirs(self.new_weights_dir, exist_ok=True) # create weight directory
+
         for i in range(self.grid_num):
             y_train_px = y_train[:, i]
             y_train_one_hot = tf.keras.utils.to_categorical(y_train_px, self.class_num)
